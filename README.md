37d06349421d705780cc80f3b7f8b9c84ac37712
Jatudrei <yehoshua.kahan@gmail.com>
2020-06-08 00:05:36 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# StarDestroyer" 
+"# ImperialStarDestroyer" 
--- src/Hull/FwdHullSection.java
+++ src/Hull/FwdHullSection.java
@@ -1,14 +1,12 @@
 package Hull;
 
 public class FwdHullSection extends HullSection{
-    private final String SectionType = "Fwd";
-    private final int CostToBuild = 1800;
-    private final int Length = 400;
-    private final int Width = 700;
-    private final int Weight = 900;
-    private boolean Assembled = false;
 
     public FwdHullSection(String id) {
-        super(id);
+        super(id, "Fwd", 1800, 400, 150, 700, 900, false);
+    }
+
+    public void checking(){
+        System.out.println("Sonic the Hedgehog");
     }
 }
